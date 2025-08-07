73f33a3fe604c5e15554d303684a004c876d6b7f
Leyla-spec <leylasa@code.edu.az>
2025-08-07 21:32:52 +0400

    Changed css and html files

--- Project3.frontend/assets/js/script.js
+++ Project3.frontend/assets/js/script.js
@@ -13,3 +13,56 @@ function linkhide(x){
         x.style.display = "block";
     }
 }
+$(document).ready(function() {
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+let swiperCMSglobalSetting = {
+    speed: 400,
+    spaceBetween: 100,
+    autoplay: false,
+    disableOnInteraction: false,
+    effect: "fade",
+    pagination: false,
+    navigation: false,
+    loop: true,
+  };
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+//   var swiper = new Swiper(this, swiperCMSglobalSetting);
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+ slides[i].style.display = "none";
+//     slides[i].mouseenter = function() { swiper.autoplay.stop(); };
+//     slides[i].mouseleave = function() { swiper.autoplay.start();
+//   }
+}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+    $(`.mySlides`).mouseenter(function() {
+        while (slideIndex > 1) {
+            showSlides(slideIndex);
+        }
+    });
+});

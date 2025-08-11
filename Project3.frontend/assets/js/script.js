abc0e75c8a26d3b2860e077b774659a3cff01cff
Leyla-spec <leylasa@code.edu.az>
2025-08-11 16:06:20 +0400

    Update file

--- Project3.frontend/assets/js/script.js
+++ Project3.frontend/assets/js/script.js
@@ -73,3 +73,115 @@ dots.forEach((dot, index) => {
     });
 });
 
+
+const slideContainer = document.querySelector(".slider");
+const slidesFlex = document.querySelector(".slides-flex");
+const navigation = document.querySelector(".navigation");
+
+let initialX,
+    finalX,
+    leftPos = -100,
+    slideDistance,
+    interval = null,
+    slidesWidth = slidesFlex.offsetWidth,
+    numberOfSlides = slidesFlex.childElementCount,
+    counter = 0,
+    threshold = 40,
+    clicked = false;
+
+document.addEventListener("DOMContentLoaded", createInterval);
+
+slideContainer.addEventListener("mousedown", dragStart);
+slideContainer.addEventListener("touchstart", dragStart);
+slideContainer.addEventListener("touchmove", dragging);
+slideContainer.addEventListener("touchend", dragStop);
+
+function moveSlide() {
+    slidesFlex.style.left = `${leftPos * counter}%`;
+    let radioBtn = document.getElementById("radio" + (counter + 1));
+    if (radioBtn) radioBtn.checked = true;
+}
+
+function animate() {
+    counter++;
+    if (counter > (numberOfSlides - 1)) {
+        counter = 0;
+        slidesFlex.style.transition = "none";
+    } else {
+        slidesFlex.style.transition = "0.8s";
+    }
+    moveSlide();
+}
+
+function createInterval() {
+    if (!interval) {
+        interval = setInterval(animate, 5000);
+    }
+}
+
+function dragStart(e) {
+    clearInterval(interval);
+    interval = null;
+
+    if (navigation.contains(e.target)) {
+        counter = e.target.id ? parseInt(e.target.id) - 1 : counter;
+        slidesFlex.style.transition = "0.8s";
+        moveSlide();
+        createInterval();
+        return;
+    }
+
+    e.preventDefault();
+
+    slidesFlex.style.transition = "0.5s";
+    slideContainer.style.cursor = "grabbing";
+    document.body.style.cursor = "grabbing";
+
+    clicked = true;
+
+    if (e.type === "touchstart") {
+        initialX = e.touches[0].clientX;
+    } else {
+        initialX = e.clientX;
+        document.onmousemove = dragging;
+        document.onmouseup = dragStop;
+    }
+}
+
+function dragging(e) {
+    if (!clicked) return;
+
+    if (e.type === "touchmove") {
+        finalX = e.touches[0].clientX;
+    } else {
+        finalX = e.clientX;
+    }
+
+    let currentPosition = counter * leftPos;
+    slideDistance = ((initialX - finalX) / (slidesWidth / numberOfSlides)) * 100;
+
+    if (Math.abs(slideDistance) < threshold) {
+        slidesFlex.style.left = `${currentPosition - slideDistance}%`;
+    }
+}
+
+function dragStop(e) {
+    if (navigation.contains(e.target)) return;
+
+    if (finalX < initialX && counter < (numberOfSlides - 1) && slideDistance >= threshold) {
+        counter++;
+    } else if (finalX > initialX && counter > 0 && -slideDistance >= threshold) {
+        counter--;
+    }
+
+    moveSlide();
+    createInterval();
+
+    document.body.style.cursor = "default";
+    slideContainer.style.cursor = "grab";
+    initialX = undefined;
+    finalX = undefined;
+    clicked = false;
+    document.onmousemove = null;
+    document.onmouseup = null;
+}

ad78696321f59d7a798204a88a450206c54a0bd4
Leyla-spec <leylasa@code.edu.az>
2025-08-12 18:03:25 +0400

    Updated css and html flies

--- Project3.frontend/assets/js/script.js
+++ Project3.frontend/assets/js/script.js
@@ -185,3 +185,38 @@ function dragStop(e) {
     document.onmousemove = null;
     document.onmouseup = null;
 }
+
+
+window.onload = function(){
+    slideOne();
+    slideTwo();
+}
+
+// let sliderOne = document.getElementById("areaMin");
+// let sliderTwo = document.getElementById("areaMax");
+// let displayValOne = document.getElementById("range1");
+// let displayValTwo = document.getElementById("range2");
+// let minGap = 0;
+// let sliderTrack = document.querySelector(".area-range");
+// let sliderMaxValue = document.getElementById("areaMin").max;
+
+// function slideOne(){
+//     if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+//         sliderOne.value = parseInt(sliderTwo.value) - minGap;
+//     }
+//     displayValOne.textContent = sliderOne.value;
+//     fillColor();
+// }
+// function slideTwo(){
+//     if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+//         sliderTwo.value = parseInt(sliderOne.value) + minGap;
+//     }
+//     displayValTwo.textContent = sliderTwo.value;
+//     fillColor();
+// }
+// function fillColor(){
+//     percent1 = (sliderOne.value / sliderMaxValue) * 100;
+//     percent2 = (sliderTwo.value / sliderMaxValue) * 100;
+//     sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #3264fe ${percent1}% , #3264fe ${percent2}%, #dadae5 ${percent2}%)`;
+// }
+
